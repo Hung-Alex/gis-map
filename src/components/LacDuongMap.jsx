c9b72d2f935ec205c08d127ecfe64e5f3f8b99fc
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-04-01 21:36:00 +0700

    thêm dữ liệu ảo, css tooltip

--- src/components/LacDuongMap.jsx
+++ src/components/LacDuongMap.jsx
@@ -57,17 +57,32 @@ class LacDuongMap extends Component {
     // dữ liệu ảo
     const positions = [
       {
-        coords: [12.237512529881249, 108.4774921063408],
-        content:
-          "Tổ dân phố Bon Đưng II Thị trấn Lạc Dương Lạc Dương Lâm Đồng.",
+        coords: [12.216667810618665, 108.41141441516834],
+        content: "Dưng KNớ, Lạc Dương, Lâm Đồng.",
       },
       {
-        coords: [12.085738195608876, 108.63222122825454],
+        coords: [12.023884470589863, 108.43055465115052],
         content: "Tổ dân phố ABC II Thị trấn Lạc Dương Lạc Dương Lâm Đồng.",
       },
       {
         coords: [12.034675216939963, 108.36520960421076],
-        content: "Tổ dân phố XYZ II Thị trấn Lạc Dương Lạc Dương Lâm Đồng.",
+        content: "Lát ,Lạc Dương, Lâm Đồng.",
+      },
+      {
+        coords: [12.18880105400144, 108.53752665863274],
+        content: "Đạ Nhim, Lạc Dương, Lâm Đồng.",
+      },
+      {
+        coords: [12.190704303337201, 108.64343682211647],
+        content: "Đạ Chais, Lạc Dương, Lâm Đồng.",
+      },
+      {
+        coords: [12.023773363665688, 108.55944212791175],
+        content: "Đạ Sar, Lạc Dương, Lâm Đồng.",
+      },
+      {
+        coords: [12.092806913800128, 108.55194621421649],
+        content: "Đạ Nhim 2, Lạc Dương, Lâm Đồng.",
       },
     ];
     return (
