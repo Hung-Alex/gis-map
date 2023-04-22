1e7992eed914b97386283f978501e9c34e426aff
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-04-22 18:18:14 +0700

    them sidebar

--- src/components/LacDuongMap.jsx
+++ src/components/LacDuongMap.jsx
@@ -100,6 +100,55 @@ class LacDuongMap extends Component {
       <div className="container-fluid text-center bg-light">
         <h1>Bản đồ huyện Lạc Dương</h1>
         <div className="text-center">
+          {/* navbar */}
+          <div className="container-fluid sidebarCss">
+            <div className="row">
+              <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+                <div className="sidebar-sticky">
+                  <ul className="nav flex-column">
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active"
+                        href="https://github.com/quanghuybest2k2/PetShop"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        Nông hộ
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        href="https://github.com/quanghuybest2k2/PetShop"
+                        rel="noopener noreferrer"
+                      >
+                        Nông sản
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        href="https://github.com/quanghuybest2k2/PetShop"
+                        rel="noopener noreferrer"
+                      >
+                        Bổ sung 1
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        href="https://github.com/quanghuybest2k2/PetShop"
+                        rel="noopener noreferrer"
+                      >
+                        Bổ sung 2
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+          {/* end navbar */}
           <MapContainer
             style={{ height: "90vh", width: "auto" }}
             zoom={11}
