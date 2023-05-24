73c2204a1fe467e01bf80f987db60ac1ebb0689f
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-05-24 10:30:57 +0700

    đổi post thành get

--- src/Services/CategoryService.js
+++ src/Services/CategoryService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.headers.get["Content-Type"] = "application/json";
+axios.defaults.headers.get["Accept"] = "application/json";
 
 export async function getCategories() {
   try {
