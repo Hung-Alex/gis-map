2a479e0888ad6c73005448e402a70032a6fa6522
Hung-Alex <alexanderpham59@gmail.com>
2023-05-25 14:52:02 +0700

    function compo

--- src/components/LacDuongMap.jsx
+++ src/components/LacDuongMap.jsx
@@ -1,7 +1,4 @@
-/*
-package: https://www.npmjs.com/package/react-leaflet-heatmap-layer-v3
-*/
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, GeoJSON, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,226 +11,187 @@ import { plantPoints } from "../data/PlantData/plant.js";
 import SlideBarMenu from "./Layout/SlideBarMenu";
 import Control from "react-leaflet-custom-control";
 import { categoriesTree } from "../data/PlantData/Category";
-// import Navbar from "./Layout/Navbar";
 
-class LacDuongMap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      plantType: "all",
-    };
-  }
-  state = { color: "#9f224e" };
-  // colors = ["#49932e", "#77ac50", "#98bf6c", "#6ec1c6", "#2d4c47", "#3e8e2f"];
-  colors = ["red", "blue", "yellow", "orange", "white", "pink"];
-  townColor = {
+const LacDuongMap = () => {
+  const [plantType, setPlantType] = useState("all");
+  const [color, setColor] = useState("#9f224e");
+  const [townColor] = useState({
     "Đạ Chais": "#49932e",
     "Đạ Nhim": "#77ac50",
     "Đạ Sar": "#98bf6c",
     "Đưng K'Nớ": "#6ec1c6",
     "TT. Lạc Dương": "#2d4c47",
     "Xã Lát": "#3e8e2f",
-  };
+  });
 
-  componentDidMount() {
-
-    console.log(mapData);
-    
-  }
-
-  
-
-
-
-  countryStyle = {
+  const countryStyle = {
     fillColor: "red",
     fillOpacity: 0.5,
     color: "black",
     weight: 2,
   };
 
-  printMesssageToConsole = (event) => {
+  const printMesssageToConsole = () => {
     console.log("Clicked");
   };
 
-  changeCountryColor = (event) => {
+  const changeCountryColor = (event) => {
     event.target.setStyle({
       color: "#2e394f",
-      fillColor: this.state.color,
-      fillOpacity: 1, // 0 hoặc 1
+      fillColor: color,
+      fillOpacity: 1,
     });
   };
 
-  onEachCountry = (country, layer) => {
-    const townName = country.properties.NAME_4; // lấy tên của xã, thị trấn
-    console.log(townName); // log ra dãy tên xã bên console
+  const onEachCountry = (country, layer) => {
+    const townName = country.properties.NAME_4;
     layer.bindTooltip(townName, { className: "my-tooltip" });
 
-    // end xử lý màu sắc random
-    layer.options.fillColor = this.townColor[townName]; //0
+    layer.options.fillColor = townColor[townName];
     layer.on({
-      click: this.changeCountryColor,
+      click: changeCountryColor,
     });
   };
 
-  colorChange = (event) => {
-    this.setState({ color: event.target.value });
+  const colorChange = (event) => {
+    setColor(event.target.value);
   };
 
-  render() {
-    const center = [12.140721657691136, 108.54228437271122];
-    // dữ liệu ảo
-    const positions = [
-      {
-        coords: [12.216667810618665, 108.41141441516834],
-        content: "Dưng KNớ, Lạc Dương, Lâm Đồng.",
-      },
-      {
-        coords: [12.023884470589863, 108.43055465115052],
-        content: "Tổ dân phố ABC II Thị trấn Lạc Dương Lạc Dương Lâm Đồng.",
-      },
-      {
-        coords: [12.034675216939963, 108.36520960421076],
-        content: "Lát ,Lạc Dương, Lâm Đồng.",
-      },
-      {
-        coords: [12.18880105400144, 108.53752665863274],
-        content: "Đạ Nhim, Lạc Dương, Lâm Đồng.",
-      },
-      {
-        coords: [12.190704303337201, 108.64343682211647],
-        content: "Đạ Chais, Lạc Dương, Lâm Đồng.",
-      },
-      {
-        coords: [12.023773363665688, 108.55944212791175],
-        content: "Đạ Sar, Lạc Dương, Lâm Đồng.",
-      },
-      {
-        coords: [12.092806913800128, 108.55194621421649],
-        content: "Đạ Nhim 2, Lạc Dương, Lâm Đồng.",
-      },
-    ];
+  const center = [12.140721657691136, 108.54228437271122];
 
-    const rosePoints = plantPoints.filter((point) => point.type === "rose");
-    // rosePoints.forEach((point) => {
-    //   console.log(point.coordinates);
-    // });
-    const bananaPoints = plantPoints.filter((point) => point.type === "banana");
+  const positions = [
+    {
+      coords: [12.216667810618665, 108.41141441516834],
+      content: "Dưng KNớ, Lạc Dương, Lâm Đồng.",
+    },
+    {
+      coords: [12.023884470589863, 108.43055465115052],
+      content: "Tổ dân phố ABC II Thị trấn Lạc Dương Lạc Dương Lâm Đồng.",
+    },
+    {
+      coords: [12.034675216939963, 108.36520960421076],
+      content: "Lát ,Lạc Dương, Lâm Đồng.",
+    },
+    {
+      coords: [12.18880105400144, 108.53752665863274],
+      content: "Đạ Nhim, Lạc Dương, Lâm Đồng.",
+    },
+    {
+      coords: [12.190704303337201, 108.64343682211647],
+      content: "Đạ Chais, Lạc Dương, Lâm Đồng.",
+    },
+    {
+      coords: [12.023773363665688, 108.55944212791175],
+      content: "Đạ Sar, Lạc Dương, Lâm Đồng.",
+    },
+    {
+      coords: [12.092806913800128, 108.55194621421649],
+      content: "Đạ Nhim 2, Lạc Dương, Lâm Đồng.",
+    },
+  ];
 
-    const fishPoints = plantPoints.filter((point) => point.type === "fish");
+  const rosePoints = plantPoints.filter((point) => point.type === "rose");
+  const bananaPoints = plantPoints.filter((point) => point.type === "banana");
+  const fishPoints = plantPoints.filter((point) => point.type === "fish");
 
-    const points =
-      this.state.plantType === "all"
-        ? plantPoints
-        : this.state.plantType === "rose"
-        ? rosePoints
-        : this.state.plantType === "fish"
-        ? fishPoints
-        : bananaPoints;
+  const points =
+    plantType === "all"
+      ? plantPoints
+      : plantType === "rose"
+      ? rosePoints
+      : plantType === "fish"
+      ? fishPoints
+      : bananaPoints;
 
-    console.log(categoriesTree);
-    return (
-      <div className="container-fluid text-center bg-light">
-        <h3>Bản đồ huyện Lạc Dương</h3>
+  useEffect(() => {
+    console.log(mapData);
+  }, []);
 
-        <div className="text-center">
-          {/* navbar */}
-          {/* <Navbar /> */}
-          <div>
-            <button
-              className="btn btn-primary btn-sm me-3 mb-1"
-              onClick={() => this.setState({ plantType: "all" })}
-            >
-              Tất cả
-            </button>
-            <button
-              className="btn btn-danger btn-sm me-3 mb-1"
-              onClick={() => this.setState({ plantType: "rose" })}
-            >
-              Hoa Hồng
-            </button>
-            <button
-              className="btn btn-warning btn-sm me-3 mb-1"
-              onClick={() => this.setState({ plantType: "banana" })}
-            >
-              Chuối
-            </button>
-            <button
-              className="btn btn-success btn-sm me-3 mb-1"
-              onClick={() => this.setState({ plantType: "fish" })}
-            >
-              Cá Tầm
-            </button>
-          </div>
-          {/* end navbar */}
-          <MapContainer
-            style={{ height: "90vh", width: "auto" }}
-            zoom={11}
-            center={center}
-            minZoom={11}
-            maxZoom={11} // khóa thu phóng
-            // dragging={false} // khóa không cho di chuyển
+  return (
+    <div className="container-fluid text-center bg-light">
+      <h3>Bản đồ huyện Lạc Dương</h3>
+      <div className="text-center">
+        <div>
+          <button
+            className="btn btn-primary btn-sm me-3 mb-1"
+            onClick={() => setPlantType("all")}
           >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <HeatmapLayer
-              // fitBoundsOnLoad
-              // fitBoundsOnUpdate
-              points={points.map((point) => ({
-                lat: point.coordinates[0], // vĩ độ
-                lng: point.coordinates[1], // kinh độ
-                value: point.type,
-              }))}
-              longitudeExtractor={(m) => m.lng}
-              latitudeExtractor={(m) => m.lat}
-              intensityExtractor={(m) => m.value}
-              maxZoom={11}
-              // max={1.0}
-              blur={15} // độ mờ của điểm dữ liệu trên bản đồ
-              radius={20} // bán kính của vùng
-            />
-            <GeoJSON
-              style={this.countryStyle}
-              data={mapData}
-              onEachFeature={this.onEachCountry}
-            />
-            {/* hiển thị vị trí phân bố */}
-            {positions.map(({ coords, content }, index) => (
-              <Marker
-                key={index}
-                position={coords}
-                icon={
-                  new L.Icon({
-                    iconUrl: iconLocation,
-                    iconSize: [41, 41], // size
-                    iconAnchor: [12, 41],
-                    popupAnchor: [1, -34],
-                  })
-                }
-              >
-                <Popup>
-                  <span>Vị trí phần bố: {index + 1}</span>
-                  <br />
-                  {content}
-                </Popup>
-              </Marker>
-              /* end vị trí phân bố */
-            ))}
-            <Control prepend position="topleft">
-              <SlideBarMenu data={categoriesTree} />
-            </Control>
-          </MapContainer>
+            Tất cả
+          </button>
+          <button
+            className="btn btn-danger btn-sm me-3 mb-1"
+            onClick={() => setPlantType("rose")}
+          >
+            Hoa Hồng
+          </button>
+          <button
+            className="btn btn-warning btn-sm me-3 mb-1"
+            onClick={() => setPlantType("banana")}
+          >
+            Chuối
+          </button>
+          <button
+            className="btn btn-success btn-sm me-3 mb-1"
+            onClick={() => setPlantType("fish")}
+          >
+            Cá Tầm
+          </button>
         </div>
-        {/* <input
-          type="color"
-          value={this.state.color}
-          onChange={this.colorChange}
-        /> */}
+        <MapContainer
+          style={{ height: "90vh", width: "auto" }}
+          zoom={11}
+          center={center}
+          minZoom={11}
+          maxZoom={11}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <HeatmapLayer
+            points={points.map((point) => ({
+              lat: point.coordinates[0],
+              lng: point.coordinates[1],
+              value: point.type,
+            }))}
+            longitudeExtractor={(m) => m.lng}
+            latitudeExtractor={(m) => m.lat}
+            intensityExtractor={(m) => m.value}
+            maxZoom={11}
+            blur={15}
+            radius={20}
+          />
+          <GeoJSON
+            style={countryStyle}
+            data={mapData}
+            onEachFeature={onEachCountry}
+          />
+          {positions.map(({ coords, content }, index) => (
+            <Marker
+              key={index}
+              position={coords}
+              icon={
+                new L.Icon({
+                  iconUrl: iconLocation,
+                  iconSize: [41, 41],
+                  iconAnchor: [12, 41],
+                  popupAnchor: [1, -34],
+                })
+              }
+            >
+              <Popup>
+                <span>Vị trí phần bố: {index + 1}</span>
+                <br />
+                {content}
+              </Popup>
+            </Marker>
+          ))}
+          <Control prepend position="topleft">
+            <SlideBarMenu data={categoriesTree} />
+          </Control>
+        </MapContainer>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default LacDuongMap;
--- src/components/Layout/SlideBarMenu.jsx
+++ src/components/Layout/SlideBarMenu.jsx
@@ -1,66 +1,195 @@
-import React, { Component } from "react";
-import TreeView from "@mui/lab/TreeView";
+import React, { useEffect, useState } from "react";
+import { TreeView, TreeItem } from "@mui/lab";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import TreeItem from "@mui/lab/TreeItem";
+import Checkbox from "@mui/material/Checkbox";
 import { getCategories } from "../../Services/CategoryService";
-import { Category } from "@mui/icons-material";
 
-class SlideBarMenu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      Category:[],
-      Varities:[],
-    };
-  }
+const SlideBarMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [category, setCategory] = useState([]);
+  const [varieties, setVarieties] = useState([]);
+  const [selectedNodes, setSelectedNodes] = useState([]);
 
-  componentDidMount() {
-
-    getCategories().then(data=>{
-      if(data){
-         this.setState({Category:data.categoriesTree})
-         this.setState({Varities:data.varieties})
-          
-      }else{
-        this.setState({Category:[]})
-        this.setState({Varities:[]})
+  useEffect(() => {
+    getCategories().then((data) => {
+      if (data) {
+        setCategory(data.categoriesTree);
+        setVarieties(data.varieties);
+      } else {
+        setCategory([]);
+        setVarieties([]);
       }
-  })
-    
-  }
+    });
+  }, []);
 
-  CreateTree = (tree) => {
+  const handleNodeSelect = (event, nodeId, parentId) => {
+    const parentIndex = selectedNodes.findIndex((node) => node.id === parentId);
+
+    if (parentIndex !== -1) {
+      const selectedParent = selectedNodes[parentIndex];
+      const selectedChildren = selectedParent.children;
+
+      const childIndex = selectedChildren.findIndex(
+        (child) => child.id === nodeId
+      );
+
+      if (childIndex !== -1) {
+        selectedChildren.splice(childIndex, 1);
+      } else {
+        selectedChildren.push({ id: nodeId, children: [] });
+      }
+
+      if (selectedChildren.length === 0) {
+        selectedNodes.splice(parentIndex, 1);
+      }
+    } else {
+      const newParentNode = {
+        id: parentId,
+        children: [{ id: nodeId, children: [] }],
+      };
+
+      selectedNodes.push(newParentNode);
+    }
+
+    setSelectedNodes([...selectedNodes]);
+  };
+
+  const handleCheckboxChange = (event, nodeId, parentId) => {
+    event.stopPropagation();
+
+    const parentIndex = selectedNodes.findIndex((node) => node.id === parentId);
+
+    if (parentIndex !== -1) {
+      const selectedParent = selectedNodes[parentIndex];
+      const selectedChildren = selectedParent.children;
+
+      console.log('Thông tin node Cha:', selectedParent.id);
+     
+      selectedChildren.map((item,index)=>{
+        console.log('Thông tin node con của nó:', item.id);
+        item.children.map((item,index)=>{
+        console.log('Thông tin node con của nua:', item.id);
+
+        })
+      })
+      const childIndex = selectedChildren.findIndex(
+        (child) => child.id === nodeId
+      );
+
+      if (childIndex !== -1) {
+        selectedChildren.splice(childIndex, 1);
+      } else {
+        selectedChildren.push({ id: nodeId, children: [] });
+      }
+
+      if (selectedChildren.length === 0) {
+        selectedNodes.splice(parentIndex, 1);
+      }
+    } else {
+      const newParentNode = {
+        id: parentId,
+        children: [{ id: nodeId, children: [] }],
+      };
+
+      selectedNodes.push(newParentNode);
+    }
+
+    setSelectedNodes([...selectedNodes]);
+  };
+
+  const createTree = (tree) => {
     if (!tree || tree.length === 0) {
       return null;
     }
-    const varities = (itemid) => this.state.Varities.filter(x => x.category_id === itemid);
+
+    const getVarieties = (itemId) =>
+      varieties.filter((x) => x.categoryId === itemId);
+
     return tree.map((item) => {
-      const children = this.CreateTree(item.children);
-      const filteredVarities = varities(item.id);
-      
+      const children = createTree(item.children);
+      const filteredVarieties = getVarieties(item.id);
+
       return (
-        <TreeItem key={item.id} nodeId={item.id} label={item.name}>
-        {children}
-        {filteredVarities.map((variety) => (
-          <TreeItem key={variety.id} nodeId={variety.id} label={variety.name} />
-        ))}
-      </TreeItem>
+        <TreeItem
+          key={item.id}
+          nodeId={item.id}
+          label={
+            <div>
+              <Checkbox
+                checked={
+                  selectedNodes.some((node) => node.id === item.id) ||
+                  selectedNodes.some(
+                    (node) => node.id === item.id && node.children.length > 0
+                  )
+                }
+                onChange={(event) =>
+                  handleCheckboxChange(event, item.id, item.id)
+                }
+              />
+              {item.name}
+            </div>
+          }
+        >
+          {children}
+          {filteredVarieties.map((variety) => (
+            <TreeItem
+              key={variety.id}
+              nodeId={variety.id}
+              label={
+                <div>
+                  <Checkbox
+                    checked={selectedNodes.some(
+                      (node) =>
+                        node.id === item.id &&
+                        node.children.some((child) => child.id === variety.id)
+                    )}
+                    onChange={(event) =>
+                      handleCheckboxChange(event, variety.id, item.id)
+                    }
+                  />
+                  {variety.name}
+                </div>
+              }
+            >
+              {variety.children.map((child) => (
+                <TreeItem
+                  key={child.id}
+                  nodeId={child.id}
+                  label={
+                    <div>
+                      <Checkbox
+                        checked={selectedNodes.some(
+                          (node) =>
+                            node.id === variety.id &&
+                            node.children.some(
+                              (grandchild) => grandchild.id === child.id
+                            )
+                        )}
+                        onChange={(event) =>
+                          handleCheckboxChange(event, child.id, variety.id)
+                        }
+                      />
+                      {child.name}
+                    </div>
+                  }
+                />
+              ))}
+            </TreeItem>
+          ))}
+        </TreeItem>
       );
     });
   };
 
-  render() {
-    return (
-      <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-      >
-        {this.CreateTree(this.state.Category)}
-      </TreeView>
-    );
-  }
-}
+  return (
+    <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {createTree(category)}
+    </TreeView>
+  );
+};
 
 export default SlideBarMenu;
